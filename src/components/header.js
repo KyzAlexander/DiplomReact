ce9f0fbe2dbdf29d1ce963828b947266a2955590
K308-4\k3084 <54>
2022-03-09 15:40:08 +0300

    add header

---
+++ src/components/header.js
@@ -0,0 +1,25 @@
+import "../assets/styles/header.css";
+import logo from "../assets/images/logo.png";
+
+function Header() {
+  return (
+    <header>
+      <nav className='header'>
+        <div className='header__wrapperLogoAndTitle'>
+          <a
+            className='wrapperLogoAndTitle__logo'
+            href='http://localhost:3000/#'
+          >
+            <img src={logo} alt='site logo'></img>
+          </a>
+          <h1 className='wrapperLogoAndTitle__title'>Git Fake Alex</h1>
+        </div>
+        <div className='header__wrapperInput'>
+          <input className='wrapperInput__textField'></input>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
