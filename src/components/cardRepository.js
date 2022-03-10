28f87820cf492c4ac530646d62cff578cd0b62f8
K308-4\k3084 <54>
2022-03-10 10:04:11 +0300

    correction cardRepository

--- src/components/cardRepository.js
+++ src/components/cardRepository.js
@@ -1,16 +1,6 @@
-// import { useState } from "react";
+import "../assets/styles/cardRepository.css";
 
 function CardRepository() {
-  //   const [repos, setRepos] =
-  //     useState[
-  //       {
-  //         name: "HomeWork",
-  //         autor: "Alex",
-  //         access: "Public",
-  //         type: "JavaScript",
-  //         lastCommitData: "09.03.2022",
-  //       }
-  //     ];
   const repos = [
     {
       name: "HomeWork",
@@ -20,21 +10,22 @@ function CardRepository() {
       lastCommitData: "09.03.2022",
     },
   ];
-  return (
-    <section className='cardRepository'>
-      <div className='cardRepository__title'>
-        <h2 className='title__name' key={repos.name}>
-          {repos.name}
-        </h2>
-        <p className='title__access'></p>
-      </div>
-      <div className='cardRepository__subtitle'>
-        <p className='subtitle__type'></p>
-        <p className='subtitle__lastCommitData'></p>
-        <p className='subtitle__autor'></p>
-      </div>
-    </section>
-  );
+  let res = repos.map((item) => {
+    return (
+      <section className='cardRepository'>
+        <div className='cardRepository__title'>
+          <h2 className='title__name'>{item.name}</h2>
+          <p className='title__access'>{item.access}</p>
+        </div>
+        <div className='cardRepository__subtitle'>
+          <p className='subtitle__type'>{item.type}</p>
+          <p className='subtitle__lastCommitData'>{item.lastCommitData}</p>
+          <p className='subtitle__autor'>{item.autor}</p>
+        </div>
+      </section>
+    );
+  });
+  return <div>{res}</div>;
 }
 
 export default CardRepository;
